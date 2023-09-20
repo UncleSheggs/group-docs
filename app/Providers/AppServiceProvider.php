598a1c42a3e5884bf612c24068d9cbe4d43c4b8c
Cyril Fehintoluwa <cyrilcril@gmail.com>
2023-09-20 15:42:00 +0100

    laravel default install with breeze api

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        if (in_array(php_uname('s'), ['Windows NT'])) {
+            Schema::defaultStringLength(191);
+        }
+
+        Model::unguard();
+
+        Model::shouldBeStrict( ! $this->app->isProduction());
+    }
+}
